5cd50dfb7ad17fcb9b921e9880dac7f55a01b2f0
YusufOMA <90691334+YusufOMA@users.noreply.github.com>
2021-10-01 10:37:23 +0200

    player statics

---
+++ player statics 3.py
@@ -0,0 +1,21 @@
+name = "Yusuf "
+jumpforce = 30
+power = "invisible"
+speed = 320
+car = "Mercedes G klasse "
+livesleft = just =  6
+ammo = 151
+numberoflasers = 7
+level = 10000
+wanted = True
+
+print("name : " + str (name) + "")
+print("jumpforce :" + str (jumpforce) + "")
+print("power : " + str (power) + "")
+print("speed : " + str (speed) + "")
+print("car : " + str (car) + "")
+print("livesleft : " + str (livesleft) + "")
+print("ammo : " + str (ammo) + "")
+print("numberoflasers : " + str (numberoflasers) + "")
+print("level : " + str (level) + "")
+print("wanted : " + str (wanted) + "")
