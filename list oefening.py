fe9d7a04bd5d09621237060a0e81696e5d28c469
YusufOMA <90691334+YusufOMA@users.noreply.github.com>
2021-10-29 09:54:07 +0200

    can you handle the list

---
+++ list oefening.py
@@ -0,0 +1,9 @@
+boodschappenlijst = ["prei", "spruitjes", "aardapellen", "sla", "water"]
+x = boodschappenlijst.index("water")
+print(x)
+boodschappenlijst.append("pinda")
+print(boodschappenlijst)
+boodschappenlijst.reverse()
+print(boodschappenlijst)
+boodschappenlijst.remove("sla")
+print(boodschappenlijst)
